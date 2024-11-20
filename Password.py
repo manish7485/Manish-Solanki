9e7a66386d95569ab34d48f768773f9ea15ccbb2
Manish Solanki <67890353+manish7485@users.noreply.github.com>
2024-11-20 11:58:01 +0530

    Tell Me About My Project!
    PasswordCheaker.py

---
+++ Password.py
@@ -0,0 +1,19 @@
+# Password Cheak
+while True:
+    try:
+        Password = input("Enter Password: ")
+        
+        if Password == "Priyanka":
+            print("Correct Password!")
+            print("Phone Unlocked!")
+        
+        else:
+            print("Incorrect Password!")
+            print("Try Again!")
+        
+    
+    except SyntaxError:
+        print("Please Try Again!!")
+
+#BTW My Wife's Name Priyanka!
+#And I Love Her So Much :D
